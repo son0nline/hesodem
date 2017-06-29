2297851d69769009ebff86b7e9971b61bced515d
son0nline <son0nline@yahoo.com.vn>
2017-06-29 13:59:55 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace hesodem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStI = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnItS = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnget = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStI
+            // 
+            this.btnStI.Location = new System.Drawing.Point(178, 47);
+            this.btnStI.Name = "btnStI";
+            this.btnStI.Size = new System.Drawing.Size(75, 23);
+            this.btnStI.TabIndex = 0;
+            this.btnStI.Text = "▲";
+            this.btnStI.UseVisualStyleBackColor = true;
+            this.btnStI.Click += new System.EventHandler(this.btnStI_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(48, 21);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -1,
+            2147483647,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(205, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // btnItS
+            // 
+            this.btnItS.Location = new System.Drawing.Point(48, 47);
+            this.btnItS.Name = "btnItS";
+            this.btnItS.Size = new System.Drawing.Size(75, 23);
+            this.btnItS.TabIndex = 0;
+            this.btnItS.Text = "▼";
+            this.btnItS.UseVisualStyleBackColor = true;
+            this.btnItS.Click += new System.EventHandler(this.btnItS_Click);
+            // 
+            // pictureBox1
+            //             
+            this.pictureBox1.Location = new System.Drawing.Point(317, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 251);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnget
+            // 
+            this.btnget.Location = new System.Drawing.Point(178, 102);
+            this.btnget.Name = "btnget";
+            this.btnget.Size = new System.Drawing.Size(75, 23);
+            this.btnget.TabIndex = 4;
+            this.btnget.Text = "getlink";
+            this.btnget.UseVisualStyleBackColor = true;
+            this.btnget.Click += new System.EventHandler(this.btnget_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 140);
+            this.Controls.Add(this.btnget);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnItS);
+            this.Controls.Add(this.btnStI);
+            this.Name = "Form1";
+            this.Text = "Alphabet Number";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStI;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnItS;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnget;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesodem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show(int.MaxValue.ToString("0,0"));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //for (int i = 65; i < 65+26; i++)
+            //{
+            //    textBox1.Text += Convert.ToChar(i).ToString();
+            //}
+            //MessageBox.Show(AlphabetNumber.toAlphabetNumber(26));
+
+            //int a = AlphabetNumber.toInt("AA");
+            //MessageBox.Show(a.ToString());
+        }
+
+
+        class AlphabetNumber
+        {
+            const string cs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const int hs = 26;
+
+            public static string GetExcelColumnName(int columnNumber)
+            {
+                int sochia = columnNumber;
+                string columnName = String.Empty;
+                int sodu;
+
+                while (sochia > 0)
+                {
+                    sodu = (sochia - 1) % hs;
+                    columnName = Convert.ToChar(65 + sodu).ToString() + columnName;
+                    sochia = (int)((sochia - sodu) / hs);
+                }
+
+                return columnName;
+            }
+
+            public static Int64 toInt(string AlphabetNumberStr)
+            {
+                if (string.IsNullOrEmpty(AlphabetNumberStr))
+                {
+                    throw new System.ArgumentNullException("Is null or empty", new Exception("string is null or empty"));
+                }
+                else if (System.Text.RegularExpressions.Regex.IsMatch(AlphabetNumberStr, "[^A-Z]+"))
+                {
+                    //kiểm tra chuỗi nhập vào có đúng định dạng ko?
+                    throw new System.ArgumentException("Invalid alphabet!");
+                }
+                else
+                {
+                    int s = 0;
+                    Int64 kq = 0;
+                    string revereverseStr = Reverse(AlphabetNumberStr);
+                    for (int i = 0; i < revereverseStr.Length; i++)
+                    {
+                        s = Array.IndexOf(cs.ToArray(), revereverseStr[i]) + 1;
+                        //if (i == revereverseStr.Length - 1 && revereverseStr.Length > 1)
+                        //{
+                        //    //trường hợp A đứng đầu thì A = 1;
+                        //    // bình thường A = 0;
+                        //    s = s == 0 ? 1 : s;
+                        //}
+                        kq += s * (Int64)Math.Pow(hs, i);
+                    }
+                    return kq - 1;
+                }
+            }
+
+            public static string toAlphabetNumber(Int64 ndec)
+            {
+                if (ndec < 0)
+                {
+                    throw new System.ArgumentException("to small");
+                }
+
+                if (ndec <= 25)
+                {
+                    return cs[(int)ndec].ToString();
+                }
+
+                int soDu = 0;
+                Int64 sochia = ndec + 1;
+                List<int> kq = new List<int>();
+                while (sochia != 0)
+                {
+                    soDu = (int)(sochia % hs);
+                    sochia = sochia / hs;
+                    kq.Add(soDu);
+                }
+
+                string strAlpN = "";
+
+                kq = standard(kq);
+                for (int i = kq.Count - 1; i >= 0; i--)
+                {
+                    strAlpN += cs[kq[i] - 1].ToString();
+                }
+                return strAlpN;
+            }
+
+            static List<int> standard(List<int> arr)
+            {
+                List<int> outarr = new List<int>();
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    if (i == arr.Count - 1 && arr[i] <= 0)
+                    {
+                        break;
+                    }
+                    if (arr[i] <= 0)
+                    {
+                        arr[i] = 26;
+                        arr[i + 1] = arr[i + 1] - 1;
+                    }
+                    outarr.Add(arr[i]);
+                }
+                return outarr;
+            }
+
+            static string Reverse(string s)
+            {
+                char[] charArray = s.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+        }
+
+        private void btnStI_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.ToUpper();
+            numericUpDown1.Value = AlphabetNumber.toInt(textBox1.Text);
+        }
+
+        private void btnItS_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = AlphabetNumber.toAlphabetNumber((int)numericUpDown1.Value);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = AlphabetNumber.toAlphabetNumber((Int64)numericUpDown1.Value);
+        }
+
+        private void btnget_Click(object sender, EventArgs e)
+        {
+            string tempurl = "http://img.10fastfingers.com/badge/typing-test_1_{0}.png";
+            if (textBox1.Text.Length <= 2)
+            {
+                string url = string.Format(tempurl, textBox1.Text);
+                pictureBox1.ImageLocation = url;
+            }
+        }
+    }
+}
